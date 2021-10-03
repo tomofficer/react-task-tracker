d095fe79328712d8be4d4719f7350de56f6437ff
Tom Officer <thomasofficerjr@gmail.com>
2021-10-02 21:56:05 -0700

    Add button update, css changes

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import AddTask from './components/AddTask';
 
 const App = () => {
 
+  const [showAddTask, setShowAddTask] = useState(false);
   const [tasks, setTasks] = useState([
         {
             id: 1,
@@ -46,8 +47,9 @@ const App = () => {
 
   return (
     <div className="container">
-      <Header />
-      <AddTask onAdd={addTask}/>
+      <Header onAdd={() => setShowAddTask(!showAddTask)}
+        showAdd={showAddTask} />
+      {showAddTask && <AddTask onAdd={addTask}/>}
       {tasks.length > 0 ?
         <Tasks tasks={tasks}
           onDelete={deleteTask}
