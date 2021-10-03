32365e4decb091ecf273c6ef8f22350764a2436b
Tom Officer <thomasofficerjr@gmail.com>
2021-10-02 22:49:15 -0700

    Add footer, add About

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Header from "./components/Header";
 import Tasks from './components/Tasks';
 import AddTask from './components/AddTask';
+import Footer from './components/Footer';
+import About from './components/About';
 
 const App = () => {
 
@@ -83,16 +86,25 @@ const App = () => {
   }
 
   return (
+    <Router>
     <div className="container">
       <Header onAdd={() => setShowAddTask(!showAddTask)}
         showAdd={showAddTask} />
-      {showAddTask && <AddTask onAdd={addTask}/>}
+      
+        <Route path="/" exact render={(props) => (
+          <>
+            {showAddTask && <AddTask onAdd={addTask}/>}
       {tasks.length > 0 ?
         <Tasks tasks={tasks}
           onDelete={deleteTask}
           onToggle={toggleReminder}
-        /> : 'No Tasks To Show'}
-    </div>
+          /> : 'No Tasks To Show'}
+          </>
+        )}/>
+        <Route path="/about" component={About}/>
+      <Footer />
+      </div>
+      </Router>
   );
 }
 
